9f6dbc6805fe6f763c24b0b24a4d63e02925f6a3
Jonatan Fragoso <jonatanfragoso@gmail.com>
2023-10-10 17:10:36 -0500

    att prototipo

--- Prototipo 1/Assets/Scripts/Player/PlayerController.cs
+++ Prototipo 1/Assets/Scripts/Player/PlayerController.cs
@@ -12,6 +12,28 @@ public class PlayerController : MonoBehaviour
     private Animator anim;
     private SpriteRenderer spriteRenderer; // Referência ao SpriteRenderer do personagem
 
+    private bool run = false;
+    private bool isGrounded;
+
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private Vector2 areaBoxGround;
+
+    [SerializeField]
+    private LayerMask groundLayer;
+
+    private bool pulo;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(groundCheck.position, areaBoxGround);
+
+    }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -22,23 +44,50 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        moveDirection = new Vector2(horizontal, 0);
 
-        anim.SetFloat("Horizontal", horizontal);
+        if( horizontal != 0){
+            run = true;
+        }
+        else{
+            run = false;
+        }
+        anim.SetBool("Run", run);
 
-        // if (horizontal > 0)
-        // {
-        //     spriteRenderer.flipX = false; // Sem inversão horizontal
-        // }
-        // else if (horizontal < 0)
-        // {
-        //     spriteRenderer.flipX = true; // Inversão horizontal para trás
-        // }
+        if (horizontal > 0)
+        {
+            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z); // Sem inversão horizontal
+        }
+        else if (horizontal < 0)
+        {
+            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z); // Inversão horizontal para trás
+        }
+
+        Jump();
+
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, areaBoxGround, 0, groundLayer);
+         if(isGrounded){
+            pulo = false;
+        }
+        else{
+            pulo = true;
+        }
+        anim.SetBool("Pulo", pulo);
+    }
+
+    private void Jump(){
+        isGrounded = Physics2D.OverlapBox(groundCheck.position, areaBoxGround, 0, groundLayer);
+        if(isGrounded && Input.GetButtonDown("Jump")){
+            // rb.velocity = Vector2.up * jumpForce;
+            rb.AddForce(new Vector3(0f, jumpForce, 0f), ForceMode2D.Impulse);
+        }
+
+       
     }
 
     private void FixedUpdate()
     {
-        Vector3 movePosition = (speed * Time.fixedDeltaTime * moveDirection.normalized) + rb.position;
-        rb.MovePosition(movePosition);
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Vector3 movePosition = new Vector3(horizontal, 0f, 0f);
+        transform.position += movePosition* Time.deltaTime * speed;
     }
 }
